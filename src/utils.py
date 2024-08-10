52f8a39fc241c137e58c021732bb632e5e9a465d
SvetlanaVasilevskaya25 <vasilevckay@mail.ru>
2024-08-10 13:14:21 +0800

    load file

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+
+def load_json(path: Path) -> list[dict]:
+    """
+
+    :param path:
+    :return:
+    """
+    with open(path, encoding='utf-8') as file:
+        return json.load(file)
